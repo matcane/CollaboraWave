b12b7fe71ad6355fc044e4ac824dd3b02392abce
matcane <mateuszcane@gmail.com>
2024-02-12 15:12:22 +0100

    add template for board

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './Header.jsx';
+import Board from './Board.jsx';
 
 
 function App() {
---
+++ frontend/src/Board.jsx
@@ -0,0 +1,154 @@
+
+function Board () {
+    return(
+        <>
+        <div>
+            <ol className="board">
+                <li className="stage">
+                    <div>
+                        <div className="stage-title">
+                            <p>Stage 1</p>
+                        </div>
+                            <ol className="stage-list">
+                                <li>
+                                    <div className="card"></div>
+                                </li>
+                                <li>
+                                    <a href="https://www.google.com/">Google</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.google.com/">Google</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.google.com/">Google</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.google.com/">Google</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.google.com/">Google</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.google.com/">Google</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.google.com/">Google</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.google.com/">Google</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.google.com/">Google</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.google.com/">Google</a>
+                                </li>
+                            </ol>
+                    </div>
+                </li>
+                <li className="stage">
+                    <div>
+                        <div>
+                            <p>Stage 2</p>
+                        </div>
+                            <ol>
+                                <li>
+                                    <a href="https://www.youtube.com/">Youtube</a>
+                                </li>
+                            </ol>
+                    </div>
+                </li>
+                <li className="stage">
+                    <div>
+                        <div>
+                            <p>Stage 2</p>
+                        </div>
+                            <ol>
+                                <li>
+                                    <a href="https://www.youtube.com/">Youtube</a>
+                                </li>
+                            </ol>
+                    </div>
+                </li>
+                <li className="stage">
+                    <div>
+                        <div>
+                            <p>Stage 2</p>
+                        </div>
+                            <ol>
+                                <li>
+                                    <a href="https://www.youtube.com/">Youtube</a>
+                                </li>
+                            </ol>
+                    </div>
+                </li>
+                <li className="stage">
+                    <div>
+                        <div>
+                            <p>Stage 2</p>
+                        </div>
+                            <ol>
+                                <li>
+                                    <a href="https://www.youtube.com/">Youtube</a>
+                                </li>
+                            </ol>
+                    </div>
+                </li>
+                <li className="stage">
+                    <div>
+                        <div>
+                            <p>Stage 2</p>
+                        </div>
+                            <ol>
+                                <li>
+                                    <a href="https://www.youtube.com/">Youtube</a>
+                                </li>
+                            </ol>
+                    </div>
+                </li>
+                <li className="stage">
+                    <div>
+                        <div>
+                            <p>Stage 2</p>
+                        </div>
+                            <ol>
+                                <li>
+                                    <a href="https://www.youtube.com/">Youtube</a>
+                                </li>
+                            </ol>
+                    </div>
+                </li>
+                <li className="stage">
+                    <div>
+                        <div>
+                            <p>Stage 2</p>
+                        </div>
+                            <ol>
+                                <li>
+                                    <a href="https://www.youtube.com/">Youtube</a>
+                                </li>
+                            </ol>
+                    </div>
+                </li>
+        
+                <li className="stage">
+                    <div>
+                        <div>
+                            <p>Stage 2</p>
+                        </div>
+                            <ol>
+                                <li>
+                                    <a href="https://www.youtube.com/">Youtube</a>
+                                </li>
+                            </ol>
+                    </div>
+                </li>        
+                
+            </ol>
+        </div>
+        
+        </>
+    )
+}
+
+export default Board
